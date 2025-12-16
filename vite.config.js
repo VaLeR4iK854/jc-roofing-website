5131a16897749b8595a77aa5f3f49d39e7a150e2
VaLeR4iK854 <valer4ik854@gmail.com>
2025-12-16 16:04:12 -0300

    Fix GitHub Pages deployment workflow

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/jc-roofing-prototype/',
+  base: '/jc-roofing-website/',
   server: {
     port: 3000,
     open: true
